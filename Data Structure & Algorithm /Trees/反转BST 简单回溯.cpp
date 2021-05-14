e01206874e37108fa834d5824bf438f30f41e086
AkiyamaKunka <wong13626@gmail.com>
2021-05-14 23:47:59 +0800

    Simple inverting BST

---
+++ Data Structure & Algorithm /Trees/反转BST 简单回溯.cpp
@@ -0,0 +1,24 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+    // https://leetcode-cn.com/problems/invert-binary-tree/
+public:
+    TreeNode* invertTree(TreeNode* root) {
+        if(root == nullptr) return nullptr;
+        TreeNode * l = invertTree(root->left);
+        TreeNode * r = invertTree(root->right);
+        root->left = r;
+        root->right = l;
+        return root;
+    }
+
+};
